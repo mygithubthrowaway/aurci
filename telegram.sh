01a41e716f5d0007d1f9933c06bacb145fe52077
mygithubthrowaway <43754602+mygithubthrowaway@users.noreply.github.com>
2019-08-20 09:32:22 +0200

    fixup! more packages, spotify removed

--- telegram.sh
+++ telegram.sh
@@ -5,9 +5,9 @@ TOKEN="$1"
 CHATID="$2"
 MESSAGE="$3"
 
-MESSAGE="<strong>Travis-ci Repo Notification</strong>\n  ${MESSAGE}"
+MESSAGE="*Travis-ci Repo Notification* \n  ${MESSAGE}"
 /usr/bin/curl --silent --output /dev/null \
     --data-urlencode "chat_id=${CHATID}" \
     --data-urlencode "text=${MESSAGE}" \
-    --data-urlencode "parse_mode=HTML" \
+    --data-urlencode "parse_mode=markdown" \
     --data-urlencode "disable_web_page_preview=true" "https://api.telegram.org/bot${TOKEN}/sendMessage"
